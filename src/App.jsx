abee8a0a293bf3b3ec8e0eb49137801c82c00f1b
Sruthi <sruthimanikandan2013@gmail.com>
2025-08-20 12:42:46 +0530

    React Country App setup with Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Routes, Route, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux'
+import Login from './pages/Login';
+import Home from './pages/Home';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
+  const isAuthenticated = useSelector((s) => s.auth.isAuthenticated)
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    
+      <Routes>
+        <Route path='/' element={isAuthenticated ? <Navigate to="/home" /> : <Login/>} />
+        <Route path='/home' element={isAuthenticated ? <Home/> : <Navigate to="/" />} />
+        <Route path='*' element={<Navigate to={isAuthenticated ? '/home' : '/'}/>} />
+      </Routes>
   )
 }
-
-export default App
